a1ac0c2792d8e623ef2ed5b49514033f61c028eb
galitsky.e.o <galitsky.e.o@gmail.com>
2019-04-08 10:52:21 +0300

    config files

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+    public render = () => {
+        return (
+            <div>
+                Welcome to new App :-)
+            </div>
+        );
+    };
+};
