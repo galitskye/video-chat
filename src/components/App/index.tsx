4f49984c4bb09b40444b7a9c74cbcf606ac0a2cf
galitsky <eugen.galitsky@spryker.com>
2019-08-01 13:07:42 +0300

    basic configuration

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+import './index.css';
+
+class App extends PureComponent {
+    public render(): React.ReactNode {
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
+}
+
+export default App;
